a5d8d836aca947fb633df6a63f1c19f19598443e
unknown <madhu.madhushika@gmail.com>
2021-02-07 14:13:02 +0530

    added config files

---
+++ src/test/java/com/madhushika/configlocal/configlocal/ConfigLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhushika.configlocal.configlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
